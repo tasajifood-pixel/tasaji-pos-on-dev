149e6b088d3c0e50e0d45ce7b26d6a97312fdad3
tasajifood-pixel <canaibumariam@gmail.com>
2025-12-22 22:52:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ const sb = window.supabase.createClient(
    DOM REFS
 ===================================================== */
 const panelReport = document.getElementById("panel-report");
+/* =====================================================
+   MAIN PANELS (SSOT)
+===================================================== */
+const panelProduct       = document.getElementById("panel-product");
+const panelPayment       = document.getElementById("panel-payment");
+const panelTransactions  = document.getElementById("panel-transactions");
+const panelSettings      = document.getElementById("panel-settings");
 
 const productGrid = document.getElementById("productGrid");
 const pageInfo = document.getElementById("pageInfo");
